ed8b60f93346714528107fb90bfdf254e06568cd
ZCODES21 <toshchatbot@gmail.com>
2024-12-08 19:24:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const chalk = require("chalk");
 const { execSync } = require("child_process");
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
